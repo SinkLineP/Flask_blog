a79d83d5c2512e575bba71bdce7616a77b428fd2
SinkLineP <popovartem505@gmail.com>
2019-10-06 19:34:35 +0300

    front end

--- Flask_blog/flaskblog/config.py
+++ Flask_blog/flaskblog/config.py
@@ -7,5 +7,5 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = os.environ.get('MAIL_USER')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASS')
