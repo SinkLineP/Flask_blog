5e13751767a6853179bdb45f93d58f410696ceb3
SinkLineP <popovartem505@gmail.com>
2019-10-07 21:39:17 +0300

    create button Chat

--- Flask_blog/flaskblog/main/routes.py
+++ Flask_blog/flaskblog/main/routes.py
@@ -12,6 +12,6 @@ def home():
     return render_template('home.html', posts=posts)
 
 
-@main.route("/about")
+@main.route("/chat")
 def about():
-    return render_template('about.html', title='About')
+    return render_template('about.html', title='Chat')
